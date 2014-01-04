aa9c46a9bbff790599c16654816ffac2c0e11d63
simkimsia <kimcity@gmail.com>
2014-01-04 13:29:09 +0800

    initial commit

---
+++ EstimoteDemo/src/main/java/com/estimotedemo/estimotedemo/ListBeaconsActivity.java
@@ -0,0 +1,150 @@
+package com.estimotedemo.estimotedemo;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Displays list of found beacons sorted by RSSI.
+ *
+ * @author wiktorgworek@google.com (Wiktor Gworek)
+ */
+public class ListBeaconsActivity extends Activity {
+
+  private static final String TAG = ListBeaconsActivity.class.getSimpleName();
+
+  private static final int REQUEST_ENABLE_BT = 1234;
+  private static final String ESTIMOTE_PROXIMITY_UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+  private static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region(ESTIMOTE_PROXIMITY_UUID, null, null);
+
+  private BeaconManager beaconManager;
+  private LeDeviceListAdapter adapter;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    // Configure device list.
+    adapter = new LeDeviceListAdapter(this);
+    ListView list = (ListView) findViewById(R.id.device_list);
+    list.setAdapter(adapter);
+    list.setOnItemClickListener(createOnItemClickListener());
+
+    // Configure BeaconManager.
+    beaconManager = new BeaconManager(this);
+    beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+      @Override
+      public void onBeaconsDiscovered(Region region, final List<Beacon> beacons) {
+        // Note that results are not delivered on UI thread.
+        runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+            getActionBar().setSubtitle("Found beacons: " + beacons.size());
+            adapter.replaceWith(beacons);
+          }
+        });
+      }
+    });
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.scan_menu, menu);
+    MenuItem refreshItem = menu.findItem(R.id.refresh);
+    refreshItem.setActionView(R.layout.actionbar_indeterminate_progress);
+    return true;
+  }
+
+  @Override
+  protected void onDestroy() {
+    beaconManager.disconnect();
+
+    super.onDestroy();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+
+    // Check if device supports Bluetooth Low Energy.
+    if (!beaconManager.hasBluetooth()) {
+      Toast.makeText(this, "Device does not have Bluetooth Low Energy", Toast.LENGTH_LONG).show();
+      return;
+    }
+
+    // If Bluetooth is not enabled, let user enable it.
+    if (!beaconManager.isBluetoothEnabled()) {
+      Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+      startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    } else {
+      connectToService();
+    }
+  }
+
+  @Override
+  protected void onStop() {
+    try {
+      beaconManager.stopRanging(ALL_ESTIMOTE_BEACONS_REGION);
+    } catch (RemoteException e) {
+      Log.d(TAG, "Error while stopping ranging", e);
+    }
+
+    super.onStop();
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == REQUEST_ENABLE_BT) {
+      if (resultCode == Activity.RESULT_OK) {
+        connectToService();
+      } else {
+        Toast.makeText(this, "Bluetooth not enabled", Toast.LENGTH_LONG).show();
+        getActionBar().setSubtitle("Bluetooth not enabled");
+      }
+    }
+    super.onActivityResult(requestCode, resultCode, data);
+  }
+
+  private void connectToService() {
+    getActionBar().setSubtitle("Scanning...");
+    adapter.replaceWith(Collections.<Beacon>emptyList());
+    beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+      @Override
+      public void onServiceReady() {
+        try {
+          beaconManager.startRanging(ALL_ESTIMOTE_BEACONS_REGION);
+        } catch (RemoteException e) {
+          Toast.makeText(ListBeaconsActivity.this, "Cannot start ranging, something terrible happened",
+              Toast.LENGTH_LONG).show();
+          Log.e(TAG, "Cannot start ranging", e);
+        }
+      }
+    });
+  }
+
+  private AdapterView.OnItemClickListener createOnItemClickListener() {
+    return new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        startActivity(DistanceBeaconActivity.createIntent(ListBeaconsActivity.this, adapter.getItem(position)));
+      }
+    };
+  }
+
+}
